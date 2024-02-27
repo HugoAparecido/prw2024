6b3c9324c29cee5e1e707c53c79dfe6d139b7353
HugoAparecido <hugoapga626@gmail.com>
2024-02-27 11:23:54 -0300

    exemplo entrada

---
+++ entrada/exe1.php
@@ -0,0 +1,16 @@
+<?php
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$nome = $_GET['nome'];
+$senhaInserida = $_GET['senha'];
+$email = $_GET['email'];
+$senha = 123;
+$num3=$num1+$num2;
+echo "o resultado da soma é $num3";
+if($senha==$senhaInserida){
+echo "<p><strong>Logado com sucesso Senhor $nome</strong>, teu email será $email a senha $senhaInserida</p>";
+}
+else {
+    echo "Senha ou email inválidos";
+    echo '<a href="entrada.html">Voltar</a>';
+}
