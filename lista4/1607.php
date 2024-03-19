a19625e6131ca4fc4897dfa7526936dbbbee1ec8
HugoAparecido <hugoapga626@gmail.com>
2024-03-19 10:54:08 -0300

    terminar a 1607

---
+++ lista4/1607.php
@@ -0,0 +1,11 @@
+<?php
+$N = intval(readline());
+$resposta = [];
+$alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+for ($i = 0; $i < $N; $i++) {
+    $entrada = explode(" ", readline());
+    $numeroMudanca= 0;
+    while($entrada[0] != $entrada[1]){
+        
+    }
+}
