99c88e891faaed1cc485474b9de5dcbf0a34f99a
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-03-20 20:25:15 -0300

    Update 1607.php

--- lista4/1607.php
+++ lista4/1607.php
@@ -1,11 +1,11 @@
 <?php
 $N = intval(readline());
-$resposta = [];
-$alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-for ($i = 0; $i < $N; $i++) {
-    $entrada = explode(" ", readline());
-    $numeroMudanca= 0;
-    while($entrada[0] != $entrada[1]){
-        
+$resposta = 0;
+while ($N--) {
+    list($stringInicial, $stringFinal) = explode(" ", readline());
+    for ($i = 0; $i < min(strlen($stringInicial), strlen($stringFinal)); $i++) {
+        $resposta += abs(ord($stringInicial[$i])-ord($stringFinal[$i]));
     }
+    echo "$resposta\n";
+    $resposta = 0;
 }
