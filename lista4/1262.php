ef733f86d9dad0efea76ace77b472638363cae61
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-03-25 18:37:12 -0300

    Update 1262.php

--- lista4/1262.php
+++ lista4/1262.php
@@ -1,5 +1,6 @@
 <?php
-while ($N = intval(fgets(STDIN))) {
-    $R = str_split(readline());
+while ($R = strval(fgets(STDIN))) {
     $P = intval(readline());
+    preg_match_all("/(W)|(R{1,$P})/", $R, $processos);
+    echo count($processos[0]) . "\n";
 }
