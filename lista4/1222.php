9cab6dd009c2a8ade2647df57e9cdfe0883ceac9
HugoAparecido <hugoapga626@gmail.com>
2024-03-26 12:21:05 -0300

    1222

--- lista4/1222.php
+++ lista4/1222.php
@@ -12,25 +12,4 @@ for($i=0;$i<$n;$i++){
         $tamLinha = strlen($palavras[$i]+1);
     }
 }
-/*while ($entrada = fgets(STDIN)) {
-    list($numeroPalavrasConto, $numeroLinha, $caracteresLinha) = explode(" ", $entrada);
-    $paginas = $linhas = 1;
-    $conto = readline();
-    $conto = explode(" ", $conto);
-    $letras = 0;
-    for ($i = 0; $i < $numeroPalavrasConto; $i++) {
-        $x = strlen($conto[$i]);
-        if (($letras + $x + 1) <= $caracteresLinha)
-            $letras += 1 + $x;
-        else {
-            ++$linhas;
-            if ($linhas > $numeroLinha) {
-                ++$paginas;
-                $linhas = 1;
-            }
-            $letras = 0;
-        }
-    }
-    echo "$paginas\n";
-}*/
 ?>
