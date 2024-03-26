4845708f604933d40f41c07efdfbc04508ed6fa8
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-03-26 16:40:59 -0300

    Update 2137.php

--- lista4/2137.php
+++ lista4/2137.php
@@ -1,8 +1,16 @@
 <?php
-while ($N = intval(fgets(STDIN))) {
-    for ($i = 0; $i < $N; $i++)
-        $numeros[$i] = readline();
-    sort($numeros);
-    for ($i = 0; $i < $N; $i++)
-        echo strval($numeros[$i]) . "\n";
+function Ordernar($codigos)
+{
+    asort($codigos);
+    return $codigos;
+}
+while ($n = intval(fgets(STDIN))) {
+    $codigos = [];
+    for ($i = 0; $i < $n; $i++) {
+        $codigo = trim(fgets(STDIN));
+        $codigos[$codigo] = $codigo;
+    }
+    $codOrdenado = Ordernar($codigos);
+    foreach ($codOrdenado as $codigo)
+        echo $codigo . "\n";
 }
