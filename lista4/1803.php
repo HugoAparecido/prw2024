701730239fcacdf6a26706f613d30f1ea20fd9f4
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-03-26 19:00:55 -0300

    Update 1803.php

--- lista4/1803.php
+++ lista4/1803.php
@@ -1,13 +1,18 @@
 <?php
 $entrada = [];
-$saida = [];
-array_push($entrada, str_split(readline()));
-array_push($entrada, str_split(readline()));
-array_push($entrada, str_split(readline()));
-array_push($entrada, str_split(readline()));
-for($i=1; $i<count($entrada)-1;$i++){
-    array_push($saida, (($entrada[1][$i]*$entrada[2][$i]+$entrada[3][$i])%257));
+array_push($entrada, readline());
+array_push($entrada, readline());
+array_push($entrada, readline());
+array_push($entrada, readline());
+$valorColuna = [];
+for ($i = 0; $i < strlen($entrada[0]); $i++) {
+    $f = intval($entrada[0][$i]) * 1000;
+    $f += intval($entrada[1][$i]) * 100;
+    $f += intval($entrada[2][$i]) * 10;
+    $f += intval($entrada[3][$i]);
+    $valorColuna[] = $f;
 }
-foreach ($saida as $saida){
-    echo $saida."\n";
+for ($i = 1; $i < count($valorColuna) - 1; $i++) {
+    echo chr(($valorColuna[0] * $valorColuna[$i] + $valorColuna[count($valorColuna) - 1]) % 257);
 }
+echo "\n";
