85f56c649ef72d1d9f08e61014114c2b950f729c
Hugo <hugoapga626@gmail.com>
2024-04-02 12:13:27 -0300

    continuar 1184.php

--- lista5/1184.php
+++ lista5/1184.php
@@ -2,16 +2,16 @@
 $operacaoChar = readline();
 $operacao = 0;
 $matriz = [];
-for ($i = 0; $i < 12; $i++)
-    for ($j = 0; $j < 12; $j++)
+for ($i = 0; $i < 3; $i++)
+    for ($j = 0; $j < 3; $j++)
         $matriz[$i][$j] = floatval(readline());
-/*foreach ($matriz as $linha) {
+foreach ($matriz as $linha) {
     foreach ($linha as $coluna)
         echo "$coluna ";
     echo "\n";
-}*/
-for ($i = 1; $i < 12; $i++)
-    for ($j = 0; $j < $i; $j++)
+}
+for ($i = 0; $i < 3; $i++)
+    for ($j = 11-$i; $j >= 0; $j--)
         $operacao += $matriz[$i][$j];
 if ($operacaoChar == 'M')
     $operacao /= 66;
