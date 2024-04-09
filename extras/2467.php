4125dfd4f2b27fbe3bfe91d442d3cb1d69c79ea2
HugoAparecido <hugoapga626@gmail.com>
2024-04-09 11:41:56 -0300

    treinando

--- extras/2467.php
+++ extras/2467.php
@@ -4,12 +4,21 @@ $N = intval($N);
 $Q = intval($Q);
 $NxN = array_pad([], $N - 1, array_pad([], $N - 1, 0));
 for ($i = 0; $i < $Q; $i++) {
-    array_push($interacao, explode(" ", intval(readline())));
-    if (intval($interacao[0]) == 1) {
-        $NxN[intval($interacao[1])] = array_fill_keys($NxN[intval($interacao[1])], intval($interacao[2]));
-    }
-    if (intval($interacao[0]) == 2) {
-        $NxN[intval($interacao[1])] = array_fill_keys($NxN[intval($interacao[1])], intval($interacao[2]));
+    $interacao[] = explode(" ", readline());
+    switch ($interacao[0]) {
+        case 1:
+            for ($j = 0; $j < $N; $j++)
+                $NxN[$interacao[1]][$j] = $interacao[2];
+            break;
+        case 2:
+            for ($i = 0; $i < $N; $i++)
+                $NxN[$i][$interacao[1]] = $interacao[2];
+            break;
+        case 3:
+            echo "";
+            break;
+        case 4:
+            break;
     }
 }
 print_r($NxN);
