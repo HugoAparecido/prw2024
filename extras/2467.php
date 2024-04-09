4cfd60252306a14f68c73bf13314c13d89eb8530
HugoAparecido <hugoapga626@gmail.com>
2024-04-09 12:12:00 -0300

    Tentando

--- extras/2467.php
+++ extras/2467.php
@@ -2,10 +2,13 @@
 list($N, $Q) = explode(" ", readline());
 $N = intval($N);
 $Q = intval($Q);
-$NxN = array_pad([], $N - 1, array_pad([], $N - 1, 0));
+$NxN = [];
+for ($i = 0; $i < $N; $i++)
+    for ($j = 0; $j < $N; $j++)
+        $NxN[$i][$j] = 0;
 for ($i = 0; $i < $Q; $i++) {
-    $interacao[] = explode(" ", readline());
-    switch ($interacao[0]) {
+    $interacao = explode(" ", readline());
+    switch (intval($interacao[0])) {
         case 1:
             for ($j = 0; $j < $N; $j++)
                 $NxN[$interacao[1]][$j] = $interacao[2];
@@ -15,9 +18,24 @@ for ($i = 0; $i < $Q; $i++) {
                 $NxN[$i][$interacao[1]] = $interacao[2];
             break;
         case 3:
-            echo "";
+            for ($j = 0; $j < $N; $j++)
+                $arrayAtual[] = $NxN[$interacao[1]][$j];
+            $arrayAtual = array_count_values($arrayAtual);
+            $maior = 0;
+            $numero = 0;
+            foreach ($arrayAtual as $numeroAtual => $ocorrencias)
+                if ($ocorrencias > $maior) {
+                    $maior = $ocorrencias;
+                    $numero = $numeroAtual;
+                }
+            print_r($arrayAtual);
             break;
         case 4:
+            for ($i = 0; $i < $N; $i++)
+                $NxN[$i][$interacao[1]] = $interacao[2];
+            $arrayAtual = $NxN[$interacao[1]][$j];
+            $arrayAtual = array_count_values($arrayAtual);
+            print_r($arrayAtual);
             break;
     }
 }
